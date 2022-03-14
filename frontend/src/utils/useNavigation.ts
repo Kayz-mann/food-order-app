73ecad0db1c6b0442501d6fea05d4b7ae4006841
Kayz-mann <kayodefredrickbalogun@gmail.com>
2022-03-14 20:19:22 +0100

    backend deployment fix

--- frontend/src/utils/useNavigation.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react'
-import { NavigationScreenProp,NavigationRoute, NavigationContext, NavigationParams } from 'react-navigation'
-
-
-export function useNavigation(){
-
-    return useContext(NavigationContext) as NavigationScreenProp<NavigationRoute, NavigationParams>
-
-}
